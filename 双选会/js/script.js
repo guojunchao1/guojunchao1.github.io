ba0a65adeede4980bd3077a8e0070f2568571628
guojunchao1 <1013189854@qq.com>
2017-12-15 16:32:23 +0800

    双选会
    完成

--- 双选会/js/script.js
+++ 双选会/js/script.js
@@ -17,3 +17,28 @@ var swiper = new Swiper('.swiper-container', {
       } 
     }
     });
+
+ //音频标签
+var mp3 = document.querySelector("#mp3");
+
+//音乐按钮
+var mp3btn = document.querySelector("#mp3btn");
+
+//暂停音乐按钮
+//mp3Btn.style.animationPlayState = 'paused'
+//旋转音乐按钮
+//mp3Btn.style.animationPlayState = 'running'
+
+mp3btn.onclick = function () {
+    if (mp3.paused) {
+        //开始旋转
+        mp3btn.style.animationPlayState = 'running';
+        //开始音频
+        mp3.play();
+    } else {
+        //暂停旋转
+        mp3btn.style.animationPlayState = 'paused';
+        //暂停音频
+        mp3.pause();
+    }
+}
